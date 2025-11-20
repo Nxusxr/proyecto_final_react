d761388bd718c84f7a88c6603ed362df91338131
Santiago López Carbini <santiago.carbini@gmail.com>
2025-11-20 00:05:25 -0300

    Ajuste para importar font-awesome

--- front/src/app/layout.js
+++ front/src/app/layout.js
@@ -10,6 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+      </head>
       <body>
         {/* Componente de Navegación Global */}
         <Nav /> 
